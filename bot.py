a197b38bb53b2ddeb75b5603bb988e4c89551106
Tim Havelka <tim.havelka@gmail.com>
2020-12-04 00:51:44 -0600

    Add list command, create guild directory

--- bot.py
+++ bot.py
@@ -18,20 +18,30 @@ last_play = None
 @bot.event
 async def on_ready():
     print('Logged on as {0}!'.format(bot.user))
+    os.system('mkdir ./sounds')
+    for guild in bot.guilds:
+        os.system(f'mkdir ./sounds/{guild.id}')
 
+@bot.event
+async def on_guild_join(guild):
+    print(f'Joined guild: {guild.name}')
+    os.system(f'mkdir ./sounds/{guild.id}')
 
 @bot.command()
 async def play(ctx, *, query):
     """Plays a file from the local filesystem"""
-    path = f'./sounds/{query}.mp3'
+    path = f'./sounds/{ctx.guild.id}/{query}.mp3'
     source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(path))
     ctx.voice_client.play(source, after=lambda e:print('Player error: %s' % e) if e else None)
     await asyncio.sleep(30)
     if ctx.voice_client and not ctx.voice_client.is_playing():
         await ctx.voice_client.disconnect()
 
-async def stop(ctx):
-    await ctx.voice_client.disconnect()
+@bot.command()
+async def list(ctx):
+    """Lists sounds in the guild's directory"""
+    stream = os.popen(f'ls ./sounds/{ctx.guild.id}')
+    await ctx.send(stream.read())
 
 @play.before_invoke
 async def ensure_voice(ctx):
