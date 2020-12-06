4bc5297a23df52aaddcaad347649f304806a6e91
Tim Havelka <tim.havelka@gmail.com>
2020-12-06 17:56:35 -0600

    Safer i/o handling and error log

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import os
-import random
+from os import path
 import asyncio
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
 
 
 load_dotenv()
@@ -13,26 +14,40 @@ PREFIX = 's!'
 
 # Client declaration
 bot = commands.Bot(command_prefix='s!')
-last_play = None
 
 @bot.event
 async def on_ready():
     print('Logged on as {0}!'.format(bot.user))
-    os.system('mkdir ./sounds')
-    os.system('mkdir ./sounds/tmp')
+    if not path.exists('sounds'):
+        os.mkdir('sounds')
+    if not path.exists('sounds/tmp'):
+        os.mkdir('sounds/tmp')
     for guild in bot.guilds:
-        os.system(f'mkdir ./sounds/{guild.id}')
-
+        if not path.exists(f'sounds/{guild.id}'):
+            os.mkdir('sounds/{guild.id}')
 @bot.event
 async def on_guild_join(guild):
     print(f'Joined guild: {guild.name}')
-    os.system(f'mkdir ./sounds/{guild.id}')
+    if not path.exists(f'sounds/{guild.id}'):
+        os.mkdir('sounds/{guild.id}')
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'{datetime.now().date()} - {datetime.now().time()}\n')
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
 
 @bot.command()
 async def play(ctx, *, query):
     """Plays a file from the guild's sounds folder. Format: `s!play {name}`"""
-    path = f'./sounds/{ctx.guild.id}/{query}.mp3'
-    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(path))
+    filepath = f'./sounds/{ctx.guild.id}/{query}.mp3'
+    if not path.exists(filepath):
+        await ctx.send(f'Sound {query} not found')
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(filepath))
     ctx.voice_client.play(source, after=lambda e:print('Player error: %s' % e) if e else None)
     await asyncio.sleep(30)
     if ctx.voice_client and not ctx.voice_client.is_playing():
@@ -45,6 +60,10 @@ async def upload(ctx, *, name):
         await ctx.send("Attachment required")
     elif not name:
         await ctx.send("Name is required")
+    elif len(name.split()) > 1:
+        await ctx.send("Name must be one word")
+    elif path.exists(f'./sounds/{ctx.guild.id}/{name.strip()}.mp3'):
+        await ctx.send(f'A sound with name {name} already exists')
     else:
         attachment = ctx.message.attachments[0]
         tmp = f'./sounds/tmp/{attachment.filename}'
@@ -57,8 +76,13 @@ async def upload(ctx, *, name):
 @bot.command()
 async def list(ctx):
     """Lists sounds in the guild's directory"""
-    stream = os.popen(f'ls ./sounds/{ctx.guild.id}')
-    await ctx.send(stream.read())
+    if path.exists(f'sounds/{ctx.guild.id}'):
+        files = os.listdir(f'sounds/{ctx.guild.id}')
+        if len(files) == 0:
+            await ctx.send('Sounds directory is empty. Type s!help to see how to upload new sound files.')
+        else:
+            files.sort()
+            await ctx.send(', '.join(files))
 
 @play.before_invoke
 async def ensure_voice(ctx):
